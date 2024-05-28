f95f7fefbca713ced6a5a11d0671cd34d5b875a1
Dan Duran <95761555+Dan-Duran@users.noreply.github.com>
2024-05-28 09:00:09 -0400

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,5 @@
+# Add your VirusTotal API key and endpoints here 
+API_KEY = 'YOUR_API_KEY'
+API_URL_IP = 'https://www.virustotal.com/api/v3/ip_addresses/'
+API_URL_DOMAIN = 'https://www.virustotal.com/api/v3/domains/'
+
