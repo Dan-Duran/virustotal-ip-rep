ba09cc6e0f72cbf87a84b448a2c4925265a16929
Dan Duran <95761555+Dan-Duran@users.noreply.github.com>
2024-05-28 09:00:28 -0400

    Create vt.py

---
+++ vt.py
@@ -0,0 +1,32 @@
+import sys
+from includes.single_ip import display_ip_info
+from includes.single_domain import display_domain_info
+from includes.ip_domain_list import process_ip_or_domain_list
+
+def main():
+    while True:
+        print("\n----------------")
+        print("MENU")
+        print("1. Single IP Address: All info of a single IP")
+        print("2. Single Domain: All info of a single domain")
+        print("3. IP/Domain List: reputation score, country, Stats for each IP/Domain in the list")
+        print("4. Exit")
+        print("----------------\n")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            ip = input("Enter IP address: ")
+            display_ip_info(ip)
+        elif choice == "2":
+            domain = input("Enter domain: ")
+            display_domain_info(domain)
+        elif choice == "3":
+            file_path = input("Type the input file (Example: input.txt): ")
+            process_ip_or_domain_list(file_path)
+        elif choice == "4":
+            sys.exit(0)
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
